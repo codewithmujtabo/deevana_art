89c35a1d8f79ffee1669d1f024eecee9e9b027cd
Mujtabo Saidamirzoda <s.mujtabo05@gmail.com>
2024-04-19 21:18:13 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "great-vibes": ["Great Vibes", "cursive"],
+        "eb-garamond": ["EB Garamond", "serif"],
+        ibm: ["IBM Plex Sans", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
