bb9b3741a13af7eff2b8d362702613db26bc99dd
Mujtabo Saidamirzoda <s.mujtabo05@gmail.com>
2024-05-13 22:35:33 +0700

    tenth commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,17 +34,19 @@ navLink.forEach((n) => n.addEventListener("click", linkAction));
 window.onscroll = function () {
   const header = document.querySelector("header");
   const fixedNav = header.offsetTop;
-  const toTop = document.querySelector("#to-top");
+  const toTops = document.querySelectorAll("#to-top");
 
-  if (window.pageYOffset > fixedNav) {
-    header.classList.add("navbar-fixed");
-    toTop.classList.remove("hidden");
-    toTop.classList.add("flex");
-  } else {
-    header.classList.remove("navbar-fixed");
-    toTop.classList.remove("flex");
-    toTop.classList.add("hidden");
-  }
+  toTops.forEach(function (toTop) {
+    if (window.pageYOffset > fixedNav) {
+      header.classList.add("navbar-fixed");
+      toTop.classList.remove("hidden");
+      toTop.classList.add("flex");
+    } else {
+      header.classList.remove("navbar-fixed");
+      toTop.classList.remove("flex");
+      toTop.classList.add("hidden");
+    }
+  });
 };
 
 /*==================== Carousel ====================*/
